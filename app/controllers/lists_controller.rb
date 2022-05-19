df73d3ee3519b285af85ebe5db3d42ce653f9c9c
Meghan Johnson <meghan.h.johnson@gmail.com>
2022-05-19 16:52:13 +0200

    added bookmarks and css

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,8 +9,11 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    @list.save
-    redirect_to list_path(@list)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
   def show
