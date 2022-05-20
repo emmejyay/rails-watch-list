b08bbeb2bb18160da2f20e797114bd5bc1695db5
Meghan Johnson <meghan.h.johnson@gmail.com>
2022-05-20 12:09:58 +0200

    Add dotenv - Protect my secret data in .env file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'lists#index'
+  root to: 'lists#index' #to always go to the index page if a page is not specified
   resources :lists, only: [ :index, :show, :new, :create ] do
     resources :bookmarks, only: [ :new, :create, ]
   end
